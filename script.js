2a7fa842ecdfffe35e11ff7c0ffe10e6544067e7
thaynasaraiva <thaynasaraiva99@gmail.com>
2023-02-23 16:02:08 -0300

    Conversor de temperatura

---
+++ script.js
@@ -0,0 +1,9 @@
+const celsiusInput = document.getElementById("celsius");
+const fahrenheitInput = document.getElementById("fahrenheit");
+const converterButton = document.getElementById("btnConverter");
+
+converterButton.addEventListener("click", () => {
+  const celsius = parseFloat(celsiusInput.value);
+  const fahrenheit = (celsius * 1.8) + 32;
+  fahrenheitInput.value = fahrenheit.toFixed(2);
+});
